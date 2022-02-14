da1881a13d915e7eb7fa8800d00e834e3333fcee
333736 <90184972+NMbo@users.noreply.github.com>
2022-02-14 10:39:56 +0100

    Opdracht 6.2 klaar

--- register.php
+++ register.php
@@ -1,1 +1,17 @@
-register
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <form action="./index.php?content=register_script" method="post">
+                <div class="form-group">
+                    <label for="InputEmail">Vul hier uw email in:</label>
+                    <input name="email" type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp">
+                    <br>
+                </div>
+                <button type="submit" class="btn btn-secondary btn-lg">Registeer</button>
+            </form>
+        </div>
+        <div class="col-12 col-sm-6">
+            <img src="./img/kersen.png" alt="kersen" class="w-25 mx-auto d-block">
+        </div>
+    </div>
+</div>
