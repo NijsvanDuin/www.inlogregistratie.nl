ec8236376596865f08049e870893ab165e7b1e0e
333736 <90184972+NMbo@users.noreply.github.com>
2022-02-21 08:59:35 +0100

    Database backup

---
+++ db/inlog-registatie.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 21 feb 2022 om 07:56
+-- Serverversie: 5.7.31
+-- PHP-versie: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inlog-registatie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `userrole` enum('root','admin','customer','moderator') NOT NULL DEFAULT 'customer',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `register`
+--
+
+INSERT INTO `register` (`id`, `email`, `password`, `userrole`) VALUES
+(1, '333736@student.mboutrecht.nl', 'geheim', 'customer');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
