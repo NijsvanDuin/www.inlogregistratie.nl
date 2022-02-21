2a6973bed3a7f4d1a88bbd9c6acd6bce2a57a8e9
333736 <90184972+NMbo@users.noreply.github.com>
2022-02-21 11:03:32 +0100

    DB backup 2.0

--- db/inlog-registatie.sql
+++ db/inlog-registatie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Gegenereerd op: 21 feb 2022 om 07:56
+-- Gegenereerd op: 21 feb 2022 om 10:02
 -- Serverversie: 5.7.31
 -- PHP-versie: 7.3.21
 
@@ -34,14 +34,65 @@ CREATE TABLE IF NOT EXISTS `register` (
   `password` varchar(60) NOT NULL,
   `userrole` enum('root','admin','customer','moderator') NOT NULL DEFAULT 'customer',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `register`
 --
 
 INSERT INTO `register` (`id`, `email`, `password`, `userrole`) VALUES
-(1, '333736@student.mboutrecht.nl', 'geheim', 'customer');
+(1, '333736@student.mboutrecht.nl', 'geheim', 'customer'),
+(14, 'agree@f', '$2y$10$OxUWecj7/fufDOC8tEYoq.Q5Ft1E96G.rhS.dj7Wb8glcSSemj0OG', 'customer');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Gegenereerd op: 21 feb 2022 om 10:02
+-- Serverversie: 5.7.31
+-- PHP-versie: 7.3.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inlog-registatie`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `register`
+--
+
+DROP TABLE IF EXISTS `register`;
+CREATE TABLE IF NOT EXISTS `register` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(300) NOT NULL,
+  `password` varchar(60) NOT NULL,
+  `userrole` enum('root','admin','customer','moderator') NOT NULL DEFAULT 'customer',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `register`
+--
+
+INSERT INTO `register` (`id`, `email`, `password`, `userrole`) VALUES
+(1, '333736@student.mboutrecht.nl', 'geheim', 'customer'),
+(14, 'agree@f', '$2y$10$OxUWecj7/fufDOC8tEYoq.Q5Ft1E96G.rhS.dj7Wb8glcSSemj0OG', 'customer');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
