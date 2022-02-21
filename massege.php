73242184d6beb463298d394b636904d799de8700
333736 <90184972+NMbo@users.noreply.github.com>
2022-02-21 09:50:27 +0100

    Opdracht 6.5 klaar

--- massege.php
+++ massege.php
@@ -1,18 +1,32 @@
 <?php
-    $alert = (isset($_GET["alert"]))? $_GET["alert"]: "default";
+$alert = (isset($_GET["alert"])) ? $_GET["alert"] : "default";
 
-    switch($alert) {
-        case "no-email":
-            echo '<div class="alert alert-warning mt-5 w-50 mx-auto" role="alert">
+switch ($alert) {
+    case "no-email":
+        echo '<div class="alert alert-warning mt-5 w-50 mx-auto" role="alert">
             U heeft geen email ingevoert, probeer opnieuw...
           </div>';
-          header("Refresh: 3; ./index.php?content=register");
+        header("Refresh: 3; ./index.php?content=register");
         break;
-        case "":
-
+    case "emailexist":
+        echo '<div class="alert alert-warning mt-5 w-50 mx-auto" role="alert">
+            Het door u ingevulde email bestaat al, probeer opnieuw met een ander email...
+          </div>';
+        header("Refresh: 3; ./index.php?content=register");
         break;
-        default:
-            header("Location: ./index.php?content=home"); 
+    case "register-error";
+        echo '<div class="alert alert-danger mt-5 w-50 mx-auto" role="alert">
+            Er is iets fout gegaan met het regitratieprosses... Probeer het nogmaals of neem contact op met 333736@student.mboutrecht.nl
+            </div>';
+        header("Refresh: 3; ./index.php?content=register");
         break;
-    }
-?>
+    case "register-succses";
+        echo '<div class="alert alert-success mt-5 w-50 mx-auto" role="alert">
+            U bent succesvol geregistreert. U ontvangt een e-mail met een activatie-link. 
+        </div>';
+        header("Refresh: 3; ./index.php?content=login");
+        break;
+    default:
+        header("Location: ./index.php?content=home");
+        break;
+}
--- register_script.php
+++ register_script.php
@@ -1,23 +1,29 @@
 <?php
-    var_dump($_POST["email"]);
 
-    if (empty($_POST["email"]))
-    {
-        header("Location: ./index.php?content=massege&alert=no-email");
+if (empty($_POST["email"])) {
+    header("Location: ./index.php?content=massege&alert=no-email");
+} else {
+    include("./connect_db.php");
+    include("./functions.php");
+
+    $email = sanitize($_POST["email"]);
+
+    $sql = "SELECT * FROM `register` WHERE `email` = '$email'";
+
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result)) {
+        header("Location: ./index.php?content=massege&alert=emailexist");
     } else {
-        include("./connect_db.php");
-        include("./functions.php");
+        $password = "geheim";
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
 
-        $email = sanitize($_POST["email"]);
-
-        $sql = "SELECT * FROM `register` WHERE `email` = '$email'";
-
-        $result = mysqli_query($conn, $sql);
-
-       if( mysqli_num_rows($result)) {
-        //hij bestaat
-       } else {
-        //hij bestaaat niet
-       }
+        $sql = "INSERT INTO `register` (`id`, `email`, `password`, `userrole`) VALUES (NULL,'$email','$password_hash','customer')";
+        if (mysqli_query($conn,$sql)) {
+            header("Location: ./index.php?content=massege&alert=register-succses");
+        } else {
+            header("Location: ./index.php?content=massege&alert=register-error");
+        }
+    
     }
-?>
+}
