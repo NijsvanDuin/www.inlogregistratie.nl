a865964e375bf381e65dab2956ca3460c2c0f454
333736 <90184972+NMbo@users.noreply.github.com>
2022-03-21 09:02:34 +0100

    Opdracht 8.1 af

--- login.php
+++ login.php
@@ -1,1 +1,22 @@
-inloggen
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-12 col-sm-6">
+            <form action="./index.php?content=login_script" method="post">
+                <div class="form-group">
+                    <label for="InputEmail">Vul hier uw email in:</label>
+                    <input name="email" type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="E-mail">
+                    <br>
+                </div>
+                <div class="form-group">
+                    <label for="InputPassword">Vul hier uw wachtwoord in:</label>
+                    <input name="password" type="password" class="form-control" id="Inputpassword" aria-describedby="passwordHelp" placeholder="Wachtwoord">
+                    <br>
+                </div>
+                <button type="submit" class="btn btn-primary btn-lg">Log In</button>
+            </form>
+        </div>
+        <div class="col-12 col-sm-6">
+            <img src="./img/kersen.png" alt="kersen" class="w-25 mx-auto d-block">
+        </div>
+    </div>
+</div>
